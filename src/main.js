dd9dae453ca441bb6a16be8f2e6223a487882b0a
coco0613 <2643500089@qq.com>
2022-01-30 09:23:21 +0800

    首页开发

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'amfe-flexible'
-import {Button, Icon} from "vant";
+import {Button, Icon, Tab, Tabs} from "vant";
 import router from "./router/index";
 import './commom/css/base.less'
 
@@ -10,6 +10,8 @@ const app  = createApp(App)
 // vant组件库的引入
 app.use(Button)
 app.use(Icon)
+app.use(Tab)
+app.use(Tabs)
 //引入组件router
 app.use(router)
 
